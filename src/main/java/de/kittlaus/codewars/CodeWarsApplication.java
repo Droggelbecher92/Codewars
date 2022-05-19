134f167ea4999468c9b154b40277e30f24d26ee2
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-05-19 14:23:30 +0200

    inital commit

---
+++ src/main/java/de/kittlaus/codewars/CodeWarsApplication.java
@@ -0,0 +1,13 @@
+package de.kittlaus.codewars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeWarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeWarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/kittlaus/codewars/CodeWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kittlaus.codewars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeWarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
